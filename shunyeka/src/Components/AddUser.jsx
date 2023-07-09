134fa1715fc76e7cf23085c61ec3fe14b2c61cc1
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-07-10 00:24:41 +0530

    css apply

--- shunyeka/src/Components/AddUser.jsx
+++ shunyeka/src/Components/AddUser.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-
+import "../style/Style.css"
 
 const AddUser = () => {
   const [formData, setFormData] = useState({ name: "", email: "", phone: "" });
+  const [show,setShow]=useState(false)
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -10,36 +11,68 @@ const AddUser = () => {
 
   return (
     <>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <br />
-        <label htmlFor="phone">Phone:</label>
-        <input
-          type="text"
-          id="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-        />
-        <br />
-        <button type="submit">Add User</button>
-      </form>
+
+   
+  {
+    !show && (
+           <div>
+            <button className="add-btn" onClick={()=> {setShow(true)}}>Add User</button>
+            </div>
+
+    )
+  }
+            
+
+
+   
+
+    {
+        show &&  <form className="container">
+
+        <div className="input-box">
+           <p>Name</p>
+           <input
+              placeholder="Enter Name"
+             type="text"
+             id="name"
+             name="name"
+             value={formData.name}
+             onChange={handleChange}
+           />
+        </div>
+           
+        <div className="input-box">
+           <p>Email</p>
+           <input
+            placeholder="Enter Email"
+             type="email"
+             id="email"
+             name="email"
+             value={formData.email}
+             onChange={handleChange}
+           />
+       </div>
+   
+       <div className="input-box">
+           <p>Phone</p>
+           <input
+            placeholder="Enter Phone No."
+             type="text"
+             id="phone"
+             name="phone"
+             value={formData.phone}
+             onChange={handleChange}
+           />
+       </div>
+   
+           <div className="btn-container">
+               <button className="submit-btn" type="submit">Submit</button>
+           </div>
+          
+         </form>
+    }
+
+     
     </>
   );
 };
